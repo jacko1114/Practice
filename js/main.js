ec2e5916e2dc536cf5e2389f57892631a37f2bd7
jacko1114 <54627350+jacko1114@users.noreply.github.com>
2020-02-04 16:01:42 +0800

    修正bug

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(() => {
+$(document).ready(function(){
     $('.hamburger').click(function(){
         $(this).toggleClass('hamburger-x');
-            $('.collapse').slideToggle();
-        })
+        $('.collapse').slideToggle();
+     })
     $(window).resize(function(){
         let w = $(window).width();
-        if(w > 600){
-            $('.collapse').show();
-        }else{
+        if(w < 600){
             $('.collapse').hide();
-                $('.hamburger').removeClass('hamburger-x')
-            }
+            $('.hamburger').removeClass('.hamburger-x');
+        }else{
+            $('.collapse').show();
+        }
     })
 })  
