bbbaa591a203912daf74baa68bdc59b5cf90cbc8
Jacko <jacko1114@hotmail.com>
2020-02-07 21:09:12 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
     $('.hamburger').click(function(){
-        $(this).toggleClass('hamburger-x');
-        $('.collapse').slideToggle();
-     })
+      $(this).toggleClass('hamburger-x');
+      $('.collapse').slideToggle();
+  })
     $(window).resize(function(){
-        let w = $(window).width();
-        if(w < 600){
-            $('.collapse').hide();
-            $('.hamburger').removeClass('hamburger-x');
-        }else{
-            $('.collapse').show();
-        }
+      let w = $(window).width();
+      if(w < 600){
+          $('.collapse').hide();
+          $('.hamburger').removeClass('hamburger-x');
+      }else{
+          $('.collapse').show();
+      }
     })  
     //點擊移動到該處效果
     $('nav').find('a').on("click",function(e){
       e.preventDefault();
-      let target = $(this).data(target);
+      let navHeight = 60;
+      let target = $(this).data("target");
       let offset = $(target).offset().top;
       $('html,body').animate({
-        scrollTop: offset
+        // scrollTop: offset
+        scrollTop: offset - navHeight
         },200)
     });
   //回到最上面效果
@@ -32,10 +34,14 @@ $(document).ready(function(){
     $(window).scroll(function(){
        let h = $(window).scrollTop();
        if(h > 600){
-         $('#gotop').fadeIn();
+        $('#gotop').animate({
+          right: +50
+         },0).fadeIn(500);
          $('nav').addClass('active');
        }else {
-         $('#gotop').fadeOut();
+         $('#gotop').animate({
+           right: -50
+          },50).fadeOut();
          $('nav').removeClass('active');
      }
   });
