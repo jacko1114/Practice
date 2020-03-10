82c136c94f00e06bb447175c639441061d33b74d
jacko1114 <jacko1114@hotmail.com>
2020-03-10 22:31:35 +0800

    debug

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,47 @@
-const { src, dest, parallel } = require('gulp');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const csso = require('gulp-csso');
-const maps = require('gulp-sourcemaps');
-const purge = require('gulp-purgecss');
+const { src, dest, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const csso = require("gulp-csso");
+const maps = require("gulp-sourcemaps");
+const purge = require("gulp-purgecss");
 
-
-function css(){
-    return src('./css/all.scss')
+function css() {
+  return src("./css/all.scss")
     .pipe(maps.init())
-    .pipe(sass({
-        outputStyle:'expanded'
-    }))
-    .pipe(purge({
-        content:['*.html']
-    }))
-    .pipe(maps.write('./'))
-    .pipe(dest('./css/'));
+    .pipe(
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(
+      purge({
+        content: ["*.html"]
+      })
+    )
+    .pipe(maps.write("./"))
+    .pipe(dest("./css/"));
 }
 
-function css_m(){
-    return src('./css/all.scss')
+function css_m() {
+  return src("./css/all.scss")
     .pipe(maps.init())
     .pipe(sass())
-    .pipe(purge({
-        content:['*.html']
-    }))
+    .pipe(
+      purge({
+        content: ["*.html"]
+      })
+    )
     .pipe(csso())
-    .pipe(rename(function(path){
-        path.basename += '.min';
-        path.extname = '.css';
-    }))
-    .pipe(maps.write('./'))
-    .pipe(dest('./css/'))
+    .pipe(
+      rename(function(path) {
+        path.basename += ".min";
+        path.extname = ".css";
+      })
+    )
+    .pipe(maps.write("./"))
+    .pipe(dest("./css/"));
 }
 
 exports.css = css;
 exports.css_m = css_m;
-exports.default = parallel(css, css_m); 
+exports.default = parallel(css, css_m);
--- js/main.js
+++ js/main.js
@@ -1,51 +1,61 @@
-$(document).ready(function(){
-    $('.hamburger').click(function(){
-      $(this).toggleClass('hamburger-x');
-      $('.collapse').slideToggle();
-  })
-  $('.collapse').hide();
-    $(window).resize(function(){
-      let w = $(window).width();
-      if(w < 720){
-          $('.collapse').hide();
-          $('.hamburger').removeClass('hamburger-x');
-      }else{
-          $('.collapse').show();
-      }
-    })  
-    //點擊移動到該處效果
-    $('nav').find('a').on("click",function(e){
+$(document).ready(function() {
+  $(".hamburger").click(function() {
+    $(this).toggleClass("hamburger-x");
+    $(".collapse").slideToggle();
+  });
+  $(window).resize(function() {
+    let w = $(window).width();
+    if (w < 714) {
+      $(".collapse").hide();
+      $(".hamburger").removeClass("hamburger-x");
+    } else {
+      $(".collapse").show();
+    }
+  });
+  //點擊移動到該處效果
+  $("nav")
+    .find("a")
+    .on("click", function(e) {
       e.preventDefault();
       let navHeight = 60;
       let target = $(this).data("target");
       let offset = $(target).offset().top;
-      $('html,body').animate({
-        // scrollTop: offset
-        scrollTop: offset - navHeight
-        },200)
-      $('.collapse').hide();
-      $('.hamburger').removeClass('hamburger-x');
+      $("html,body").animate(
+        {
+          scrollTop: offset - navHeight
+        },
+        500
+      );
+      $(this)
+        .siblings()
+        .removeClass("active");
+      $(this).addClass("active");
+      let w = $(window).width();
+      if (w < 720) {
+        $(".collapse").hide();
+        $(".hamburger").removeClass("hamburger-x");
+      }
     });
   //回到最上面效果
-    $('#gotop').on("click",function(e){
-      e.preventDefault();
-      $('html,body').animate({
+  $("#gotop").on("click", function(e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
         scrollTop: 0
-        },200)
-    });
-  //判斷gotop出現時機
-    $(window).scroll(function(){
-       let h = $(window).scrollTop();
-       if(h > 600){
-        $('#gotop').animate({
-          right: +50
-         },0).fadeIn("slow");
-         $('nav').addClass('active');
-       }else {
-         $('#gotop').animate({
-           right: -50
-          },50).fadeOut();
-         $('nav').removeClass('active');
-     }
+      },
+      200
+    );
   });
-})
+  //判斷gotop出現時機
+  $(window).scroll(function() {
+    let h = $(window).scrollTop();
+    if (h > 60) {
+      $("#gotop").show();
+    } else {
+      $("#gotop").hide();
+      $("nav")
+        .find("a")
+        .removeClass("active");
+    }
+  });
+});
